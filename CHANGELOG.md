8ad03bcacd30da8a42e352b21b2607973d077930
joshgrrro <57468035+joshgrrro@users.noreply.github.com>
2021-10-26 16:51:07 -0700

    Restructured project as a Unity package

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.0] - 2021-10-26
+### Added
+- CHANGELOG.md
+- package.json
+- Lexonegit.UnityTwitchChat.asmdef
+### Changed
+- All scripts now belong to the Lexonegit.UnityTwitchChat namespace
+### Removed
+- Sample scene (temporarily)
+
+## [0.1.0] - 2021-06-12
+- Last release from lexonegit
---
+++ Runtime/NetworkStreamExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lexonegit.UnityTwitchChat
+{
+
+    public static class NetworkStreamExtensionMethods
+    {
+        public static void WriteLine(this NetworkStream stream, string output, bool debug = false)
+        {
+            if (debug)
+                Debug.Log("<color=#c91b00><b>[IRC OUTPUT]</b></color> Sending command: " + output);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(output);
+            stream.Write(bytes, 0, bytes.Length);
+            stream.WriteByte((byte)'\r');
+            stream.WriteByte((byte)'\n');
+            stream.Flush();
+        }
+    }
+
+}
