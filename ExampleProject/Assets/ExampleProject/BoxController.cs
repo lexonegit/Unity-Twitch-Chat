85af9a3160b791e02cd87a8838c0d04e7f5e7e7c
Leevi <leeviftw@hotmail.com>
2021-12-02 03:54:40 +0200

    ExampleProject revision 2021

---
+++ ExampleProject/Assets/ExampleProject/BoxController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxController : MonoBehaviour
+{
+    public Text nameText; 
+    public SpriteRenderer spriteRenderer;
+    public Transform canvasTransform;
+    
+    public Color moderatorColor, vipColor, subscriberColor;
+
+    public void Initialize(Chatter chatter)
+    {
+        // Set chatter's name
+        //
+        // If chatter's display name is "font safe" then use it. Otherwise use login name.
+        // Login name is always lowercase and can only contain characters: a-z, A-Z, 0-9, _
+        //
+        nameText.text = chatter.IsDisplayNameFontSafe() ? chatter.tags.displayName : chatter.login;
+
+        // Change box color
+        //
+        if (chatter.HasBadge("moderator")) // Has moderator badge
+            spriteRenderer.color = moderatorColor;
+        else
+        if (chatter.HasBadge("vip")) // Has VIP badge
+            spriteRenderer.color = vipColor;
+        else
+        if (chatter.HasBadge("subscriber")) // Has subscriber badge
+            spriteRenderer.color = subscriberColor;
+        
+        // Detach name canvas from parent so that it doesn't rotate with the transform
+        canvasTransform.SetParent(null);
+
+        // Start jumping
+        StartCoroutine(Jump());
+    }
+
+    private void LateUpdate()
+    {
+        // Update name canvas position each frame
+        canvasTransform.position = (Vector2)transform.position + new Vector2(0, 0.8f);
+    }
+
+    private IEnumerator Jump()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(2f, 4f));
+
+            float dir = Random.value > .5f ? 1f : -1f; // Random jump direction
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10f + (Vector2.right * 3f) * dir, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ ExampleProject/Assets/TwitchIRC/ExampleListener/ExampleIRCListener.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleIRCListener : MonoBehaviour
+{
+    private TwitchIRC IRC;
+
+    public Chatter latestChatter;
+
+    private void Start()
+    {
+        // This is done just for the sake of simplicity,
+        // In your own script, you should instead have a reference 
+        // to the TwitchIRC component (inspector)
+        IRC = GameObject.Find("TwitchIRC").GetComponent<TwitchIRC>();
+
+        // Add an event listener for new chat messages
+        IRC.newChatMessageEvent.AddListener(NewMessage);
+    }
+
+    // This gets called whenever a new chat message is received
+    public void NewMessage(Chatter chatter)
+    {
+        Debug.Log(
+            "<color=cyan>New chatter object received!</color>" 
+            + " Chatter's name: " + chatter.tags.displayName
+            + " Chatter's message: " + chatter.message);
+
+        // Here are some examples on how you could use the chatter objects...
+
+        if (chatter.tags.displayName == "Lexone")
+            Debug.Log("Chat message was sent by Lexone!");
+
+        if (chatter.HasBadge("subscriber"))
+            Debug.Log("Chat message sender is a subscriber");
+
+        if (chatter.HasBadge("moderator"))
+            Debug.Log("Chat message sender is a channel moderator");
+
+        if (chatter.MessageContainsEmote("25")) //25 = Kappa emote ID
+            Debug.Log("Chat message contained the Kappa emote");
+
+        if (chatter.message == "!join")
+            Debug.Log(chatter.tags.displayName + " said !join");
+
+        // Get chatter's name color (RGBA Format)
+        //
+        Color nameColor = chatter.GetRGBAColor();
+
+        // Check if chatter's display name is "font safe"
+        //
+        // Most fonts don't support unusual characters
+        // If that's the case then you could use their login name instead (chatter.login) or use a fallback font
+        // Login name is always lowercase and can only contain characters: a-z, A-Z, 0-9, _
+        if (chatter.IsDisplayNameFontSafe())
+            Debug.Log("Chatter's displayName is font-safe (only characters: a-z, A-Z, 0-9, _)");
+
+
+        // Save latest chatter object
+        // This is just to show how the Chatter object looks like inside the Inspector
+        latestChatter = chatter;
+    }
+}
