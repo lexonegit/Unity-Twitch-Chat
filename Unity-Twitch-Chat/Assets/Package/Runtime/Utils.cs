f59f2886d901cad5b65ec26b41944a190db789a2
Leevi <leeviftw@hotmail.com>
2023-01-03 04:31:51 +0200

    Utils class

---
+++ Unity-Twitch-Chat/Assets/Package/Runtime/Utils.cs
@@ -0,0 +1,29 @@
+namespace UnityTwitchChat
+{
+    /// <summary>
+    /// https://www.ietf.org/rfc/rfc1459.txt (starting on page 42 - Replies)
+    /// </summary>
+    public enum IRCReply
+    {
+        // Twitch IRC specific codes
+        CONNECTED_TO_SERVER = 001,
+        PONG_RECEIVED = 007,
+
+        // Command codes
+        JOINED_CHANNEL = 353,
+
+        // Error codes
+        MISSING_LOGIN_INFO = 431,
+        BAD_LOGIN = 464,
+        CONNECTION_INTERRUPTED = 498,
+        NO_CONNECTION = 499,
+    }
+
+    public static class Tags
+    {
+        public static string read = "<color=#00ff00><b>[IRC READ]</b></color>";
+        public static string write = "<color=#5274ff><b>[IRC WRITE]</b></color>";
+        public static string thread = "<color=#ff5252><b>[THREAD]</b></color>";
+        public static string alert = "<color=#ffae00><b>[ALERT]</b></color>";
+    }
+}
